d8f34105d07fc5a7f6904e2f4948d88db1069899
akiokio <akio.xd@gmail.com>
2019-06-26 00:33:34 -0700

    tests(all): add initial test suite

--- src/app.js
+++ src/app.js
@@ -26,11 +26,15 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(port, err => {
-  if (err) {
-    Logger.error(err);
-    process.exit(1);
-    return;
-  }
-  console.log(`App initialized on port ${port}`);
-});
+if (!module.parent) {
+  app.listen(port, err => {
+    if (err) {
+      Logger.error(err);
+      process.exit(1);
+      return;
+    }
+    console.log(`App initialized on port ${port}`);
+  });
+}
+
+export default app;
