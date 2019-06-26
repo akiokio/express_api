bc6969d519123a291a899b861d86b7bde1c0b579
akiokio <akio.xd@gmail.com>
2019-06-25 21:39:10 -0700

    feat(downloader): add first downloader implementation

--- src/controllers/common.js
+++ src/controllers/common.js
@@ -1,3 +1,5 @@
+import { downloadFile } from "../utils/downloader";
+
 export const home = (req, res) => {
   res.send("Hello common route");
 };
@@ -8,4 +10,18 @@ export const one = (req, res) => {
 
 export const post = (req, res) => {
   res.send("Got a POST request");
+};
+
+export const downloader = async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).send("url param is required");
+  }
+
+  try {
+    const fileContent = await downloadFile(req.query.url);
+    res.send(fileContent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 };
--- src/routes/common.js
+++ src/routes/common.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { home, one, post } from "../controllers/common";
+import { home, one, post, downloader } from "../controllers/common";
 
 router.get("/", home);
 router.get("/one", one);
+router.get("/downloader", downloader);
+
 router.post("/", post);
 
 export default router;
